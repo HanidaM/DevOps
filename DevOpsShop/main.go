222c6eb30676877e3ae532f520ed6083ffb4fd66
Bauyrzhan <baurzhkk@gmail.com>
2023-09-14 20:12:48 +0600

    added templates

--- DevOpsShop/main.go
+++ DevOpsShop/main.go
@@ -1,5 +1,24 @@
-package DevOpsShop
+package main
+
+import (
+	"DevOpsShop/handlers"
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+	"net/http"
+)
 
 func main() {
 
+	r := gin.Default()
+
+	r.LoadHTMLGlob("templates/*.html")
+
+	r.GET("/register", handlers.ShowRegisterPage)
+	r.POST("/register", handlers.RegisterHandler)
+
+	r.GET("/login", handlers.ShowLoginPage)
+	r.POST("/login", handlers.LoginHandler)
+	r.POST("/logout", handlers.Logout)
+
+	http.ListenAndServe(":8080", r)
 }
