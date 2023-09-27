eec292f41f6f1c37c3ce76aeb7847f9674edf825
Bauyrzhan <baurzhkk@gmail.com>
2023-09-27 21:57:33 +0600

    task3 update

--- assignment1/task3/sshconfig.sh
+++ assignment1/task3/sshconfig.sh
@@ -1,8 +1,11 @@
 
-remote_server="kasper@172.16.33.241"
-if [ ! -f ~/.ssh/id_rsa ]; then
-    ssh-keygen -t rsa -b 4096
-fi
-ssh-copy-id "$remote_server"
+username="kasper"
+remote_server="172.16.33.241"
 
-echo "SSH confidgured!"
+if [ ! -f ~/.ssh/id_rsa ]; then
+  ssh-keygen -t rsa -b 2048
+fi
+
+ssh-copy-id "$username@$remote_server"
+
+ssh -i ~/.ssh/id_rsa "$username@$remote_server"
